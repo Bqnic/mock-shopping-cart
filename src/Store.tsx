e757da4cf565a91c211df3375e105ed0c58e5438
Bqnic <123201175+Bqnic@users.noreply.github.com>
2024-02-22 19:28:48 +0100

    display products

--- src/Store.tsx
+++ src/Store.tsx
@@ -1,9 +1,15 @@
 import { StoreProps } from "./assets/interfaces";
 export default function Store({ products }: StoreProps) {
   return (
-    <div>
+    <div className="flex justify-center flex-wrap m-10 gap-5">
       {products.map((product, index) => (
-        <div key={index}>{product.id}</div>
+        <div
+          className="bg-white w-64 h-64 flex flex-col justify-center items-center border border-black p-5"
+          key={index}
+        >
+          <p>{product.title}</p>
+          <img className="w-36 h-36" src={product.image} alt="image" />
+        </div>
       ))}
     </div>
   );
